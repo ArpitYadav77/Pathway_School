2e1b2eab89bc4652115d805485a0b1a54baec6c8
Arpit Yadav <your-email@example.com>
2026-03-11 00:06:02 +0530

    fix: eslint config for Next 15 and add Sanity fallback values for Vercel

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -3,12 +3,12 @@ import { structureTool } from "sanity/structure"
 import { schemaTypes } from "./schemaTypes"
 
 export default defineConfig({
-basePath: "/studio",
-projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-title: "Pathway School CMS",
-plugins: [structureTool()],
-schema: {
-types: schemaTypes
-}
+  basePath: "/studio",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "lesn71k7",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  title: "Pathway School CMS",
+  plugins: [structureTool()],
+  schema: {
+    types: schemaTypes,
+  },
 })
